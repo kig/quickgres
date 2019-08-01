1572f7dc3fba932cd28dbfc4c4c41e0d3f78cb91
Ilmari Heikkinen <hei@heichen.hk>
2019-08-01 19:11:57 +0800

    More comments, rename outStream to queryHandler, simplify bits

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ class Client { // The Client class wraps a connection socket to a PostgreSQL dat
         if (!(config.user && config.database)) throw Error("You need to provide both 'user' and 'database' in config"); // PostgreSQL protocol requires at least a user and database.
         this._parsedStatementCount = 1; // Counter to create unique prepared statement names.
         this._parsedStatements = {}; // Index to store prepared statements. Maps from a query string to statement name and row parser.
-        this._packet = { buf: null, head: Buffer.alloc(4), cmd: 0, length: 0, idx: 0 }; // Stores the current incoming protocol message.
-        this._outStreams = []; // Internal queue of query result handlers, called in order when receiving query results.
+        this._packet = { buf: null, head: Buffer.alloc(4), cmd: 0, length: 0, index: 0 }; // Stores the current incoming protocol message.
+        this._queryHandlers = []; // Internal queue of query result handlers, called in order when receiving query results.
         this._formatQueue = []; // Internal queue of query result formats, either Client.STRING or Client.BINARY.
         this.serverParameters = {}; // Parameters received from the PostgreSQL server on connecting. Stuff like {server_encoding: 'UTF8'}
         this.config = config; // Store the config for later use.
@@ -44,119 +44,114 @@ class Client { // The Client class wraps a connection socket to a PostgreSQL dat
     }
     onError(err) { // Deal with errors in the connection.
         if (err.message.startsWith('connect EAGAIN ')) this._connect(); // If you try to open too many UNIX sockets too quickly, the Linux syscall sends an EAGAIN interrupt asking you to try again.
-        else this._formatQueue.splice(0), this._outStreams.splice(0).forEach(s => s.reject(err)); // Clear out the query result handlers and reject them all. 
+        else this._formatQueue.splice(0), this._queryHandlers.splice(0).forEach(s => s.reject(err)); // Clear out the query result handlers and reject them all. 
     }
     onConnect() { // Initiate connection with PostgreSQL server by sending the startup packet.
         if (this.config.cancel) { // Unless this connection is for canceling a long-running request.
             this._connection.write(Buffer.concat([Buffer.from([0,0,0,16,4,210,22,46]), this.config.cancel])); // Send a CancelRequest with our backendKey cancel token.
             this._connection.destroy(); // Disconnect socket.
-            return this._outStreams[0].resolve(); // Tell the cancel promise that we're done here.
+            return this._queryHandlers[0].resolve(); // Tell the cancel promise that we're done here.
         }
+        this._connection.on('data', this.onData.bind(this)); // Start listening to what the server is saying.
         let chunks = [Buffer.from([0,0,0,0,0,3,0,0])]; // Startup message for protocol version 3.0, we fill in the first four message-length bytes later.
-        for (let n in this.config) if (n !== 'password') chunks.push(Buffer.from(`${n}\0${this.config[n]}\0`)); // Send config params to the server, except for the password. It is handled in the Authentication flow.
+        for (let n in this.config) if (n !== 'password') chunks.push(Buffer.from(`${n}\0${this.config[n]}\0`)); // Send config to the server, except for the password. It is handled in the Authentication flow.
         chunks.push(Buffer.alloc(1)); // Send a zero byte to tell the server that there are no more config params.
         const msg = Buffer.concat(chunks); // Turn the chunks into one big message buffer.
         w32(msg, msg.byteLength, 0); // Write the length of the message at the beginning of the message.
         this._connection.write(msg); // Send the startup message to the server.
-        this._connection.on('data', this.onData.bind(this)); // Start listening to what the server is saying.
     }
-    onData(buf) {
-        const packet = this._packet;
-        for (var i = 0; i < buf.byteLength;) {
-            if (packet.cmd === 0) {
-                packet.cmd = buf[i++];
-                packet.length = 0;
-                packet.index = 0;
-            } else if (packet.index < 4) {
-                packet.head[packet.index++] = buf[i++];
-                if (packet.index === 4) {
-                    packet.length = r32(packet.head, 0);
-                    packet.buf = Buffer.allocUnsafe(packet.length + 1);
-                    packet.buf[0] = packet.cmd; 
-                    packet.buf[1] = packet.head[0]; packet.buf[2] = packet.head[1]; packet.buf[3] = packet.head[2]; packet.buf[4] = packet.head[3];
+    onData(buf) { // On receiving data from the server, copy it to a packet. Once the packet is complete, pass it to the protocol parser in processPacket.
+        for (let i = 0; i < buf.byteLength;) { // Process all bytes in buf.
+            if (this._packet.cmd === 0) this._packet.cmd = buf[i++]; // Set new packet command and advance byte counter.
+            else if (this._packet.index < 4) { // Parsing packet length.
+                this._packet.head[this._packet.index++] = buf[i++]; // Copy length bytes to header buffer, advancing byte counters.
+                if (this._packet.index === 4) { // That was the last length byte.
+                    this._packet.length = r32(this._packet.head, 0); // Parse length as int32BE and store for later.
+                    this._packet.buf = Buffer.allocUnsafe(this._packet.length + 1); // Allocate a message buffer to receive the rest of the packet.
+                    this._packet.buf[0] = this._packet.cmd; // Copy the already received bytes to the start of the buffer.
+                    this._packet.buf[1] = this._packet.head[0]; this._packet.buf[2] = this._packet.head[1]; this._packet.buf[3] = this._packet.head[2]; this._packet.buf[4] = this._packet.head[3];
                 }
             }
-            if (packet.index >= 4) {
-                const copiedBytes = buf.copy(packet.buf, packet.index+1, i, i + (packet.length - packet.index));
-                packet.index += copiedBytes;
-                i += copiedBytes;
-                if (packet.index === packet.length) {
-                    this.processPacket(packet.buf, packet.cmd, packet.length, 5, this._outStreams[0], this._formatQueue[0]);
-                    packet.cmd = 0;
+            if (this._packet.index >= 4) { // If the packet header is complete, copy the packet body to the message buffer.
+                const copiedBytes = buf.copy(this._packet.buf, this._packet.index+1, i, i + (this._packet.length - this._packet.index)); // Copy buf to _packet.buf, up to the length of the packet + 1 for the command byte.
+                this._packet.index += copiedBytes; // Advance byte counters to keep track where in the packet we currently are.
+                i += copiedBytes; // Advance loop byte counter by the amount of bytes processed thus far.
+                if (this._packet.index === this._packet.length) { // If the packet is complete, process it and get ready for the next packet.
+                    this.processPacket(this._packet.buf, this._packet.cmd, this._packet.length, 5, this._queryHandlers[0], this._formatQueue[0]); // Pass packet to protocol parser.
+                    this._packet.cmd = this._packet.index = this._packet.length = 0; // Reset the packet.
                 }
             }
         }
     }
-    processPacket(buf, cmd, length, off, outStream, streamFormat) { switch(cmd) {
-        case 68: // D -- DataRow
-            outStream.stream.format = streamFormat;
-            outStream.stream.rowParser = outStream.parsed.rowParser;
-            outStream.stream.write(buf);
+    processPacket(buf, cmd, length, off, queryHandler, streamFormat) { switch(cmd) { // Process a protocol packet and write it to the output stream if needed.
+        case 68: // D -- DataRow -- Writes query result rows to the output stream and tells the output stream how to parse them.
+            queryHandler.stream.format = streamFormat; // Set the output stream's data format (either Client.STRING or Client.BINARY.)
+            queryHandler.stream.rowParser = queryHandler.parsed.rowParser; // Set the output stream's row data parser.
+            queryHandler.stream.write(buf); // Write the DataRow packet to the output stream for further processing.
             break;
-        case 100: // CopyData
-            outStream.stream.write(buf);
+        case 100: // CopyData -- Copy data rows are written directly to the output stream.
+            queryHandler.stream.write(buf); // Write the CopyData packet to the output stream for further processing.
             break;
-        case 84: // T -- RowDescription
-            outStream.parsed.rowParser = new RowParser(buf);
-        case 73: case 72: case 99: // EmptyQueryResponse / CopyOutResponse / CopyDone
-            outStream.stream.write(buf);
-        case 110: case 116: case 49: case 50: case 51: // NoData / ParameterDescription / {Parse,Bind,Close}Complete
+        case 84: // T -- RowDescription -- Describes the column names and types of a query result. Parsed into a RowParser and cached. Written to output stream as well.
+            queryHandler.parsed.rowParser = new RowParser(buf); // Create new RowParser from the packet and cache it to the current prepared statement.
+        case 73: case 72: case 99: // EmptyQueryResponse / CopyOutResponse / CopyDone -- These are written to the output stream for further processing.
+            queryHandler.stream.write(buf);
+        case 110: case 116: case 49: case 50: case 51: // NoData / ParameterDescription / {Parse,Bind,Close}Complete -- These are ignored.
             break;
-        case 67: // C -- CommandComplete
-            if (this.inQuery) this.zeroParamCmd(83); // S -- Sync
-            outStream.stream.write(buf);
+        case 67: // C -- CommandComplete -- The last DataRow is followed by a CommandComplete. Written to output stream.
+            if (this.inQuery) this.zeroParamCmd(83); // S -- Sync -- In startQuery-getResult partial queries we need to sync after the command is complete, this commits the query's implicit transaction.
+            queryHandler.stream.write(buf); // Write the complete to output stream to let it know the query is finished.
             break;
-        case 115: case 71: case 87: // PortalSuspended / CopyInResponse / CopyBothResponse
-            outStream.stream.write(buf); // Pass the results to either query.getResult stream or CopyIn stream.
-            this._outStreams.shift(); // Advance _outStreams (note that the streamFormat is still valid so we don't shift _formatQueue).
-            outStream.resolve(outStream.stream); // Resolve the partial query or CopyInResponse promise.
+        case 115: case 71: case 87: // PortalSuspended / CopyInResponse / CopyBothResponse -- Two-part query flow messages. Resolve promises and get written to the output stream.
+            queryHandler.stream.write(buf); // Pass the results to either query.getResult stream or CopyIn stream.
+            this._queryHandlers.shift(); // Advance _queryHandlers (note that the streamFormat is still valid so we don't shift _formatQueue).
+            queryHandler.resolve(queryHandler.stream); // Resolve the partial query or CopyInResponse promise.
             break;
-        case 90: // Z -- ReadyForQuery
-            this.inQuery = null;
-            this._outStreams.shift();
-            this._formatQueue.shift();
-            if (outStream) outStream.resolve(outStream.stream);
+        case 90: // Z -- ReadyForQuery -- Done with the current query, resolve it. Received at the start of the connection, after a Sync, or after an Error.
+            this.inQuery = null; // End partial query.
+            this._queryHandlers.shift(); // Advance query output streams.
+            this._formatQueue.shift(); // Advance query formats as well, the next query may have a different format.
+            queryHandler.resolve(queryHandler.stream); // Resolve the current query's promise.
             break;
-        case 69: // E -- Error
-            this._outStreams[0] = null; // Error is followed by ReadyForQuery, this will eat that.
-            this._formatQueue[0] = null; // Format needs to be eaten too.
-            if (outStream) outStream.reject(Error(`${buf[off]} ${buf.toString('utf8', off+1, off+length-4).replace(/\0/g, ' ')}`));
+        case 69: // E -- Error -- There was an error in handling the query, reject the current queryHandler promise.
+            this._queryHandlers[0] = {resolve: function() {}}; // Error is followed by ReadyForQuery, this will eat that. _formatQueue[0] doesn't require overwriting.
+            queryHandler.reject(Error(`${buf[off]} ${buf.toString('utf8', off+1, off+length-4).replace(/\0/g, ' ')}`)); // Reject the current query with the error message from the server.
             break;
-        case 83: // S -- ParameterStatus
-            const [key, value] = buf.toString('utf8', off, off + length - 5).split('\0');
-            this.serverParameters[key] = value;
+        case 83: // S -- ParameterStatus -- Server parameters are received at the start of the connection, but may also arrive at any time.
+            const [key, value] = buf.toString('utf8', off, off + length - 5).split('\0'); // Split the parameter into a key-value pair.
+            this.serverParameters[key] = value; // Store the received parameters to serverParameters in case they're useful.
             break;
-        case 82: // R -- Authentication
-            const authResult = r32(buf, off); off += 4;
-            if (authResult === 0) this.authenticationOk = true;
-            else if (authResult === 3) { // 3 -- AuthenticationCleartextPassword
-                if (this.config.password === undefined) throw Error("No password supplied");
-                this.authResponse(Buffer.from(this.config.password + '\0')); 
-            } else if (authResult === 5) { // 5 -- AuthenticationMD5Password
-                if (this.config.password === undefined) throw Error("No password supplied");
-                const upHash = crypto.createHash('md5').update(this.config.password).update(this.config.user).digest('hex');
-                const salted = crypto.createHash('md5').update(upHash).update(buf.slice(off, off+4)).digest('hex');
-                this.authResponse(Buffer.from(`md5${salted}\0`)); 
-            } else { this.end(); throw(Error(`Authentication method ${authResult} not supported`)); }
+        case 82: // R -- Authentication -- Server either asks us for a password or tells that everything's A-OK, come on in friend.
+            const authResult = r32(buf, off); off += 4; // Read the auth message code and deal with it.
+            if (authResult === 0) this.authenticationOk = true; // Authentication successful.
+            else if (authResult === 3) { // 3 -- AuthenticationCleartextPassword -- Server asks us to send a password in clear text.
+                if (this.config.password === undefined) throw Error("No password supplied"); // For which we need a password.
+                this.authResponse(Buffer.from(this.config.password + '\0')); // Send auth response with the password terminated by a null byte.
+            } else if (authResult === 5) { // 5 -- AuthenticationMD5Password -- Server asks us for a MD5-hashed password.
+                if (this.config.password === undefined) throw Error("No password supplied"); // For which we need a password.
+                const upHash = crypto.createHash('md5').update(this.config.password).update(this.config.user).digest('hex'); // Hash the password and username.
+                const salted = crypto.createHash('md5').update(upHash).update(buf.slice(off, off+4)).digest('hex'); // Then hash that hash with the salt from the server.
+                this.authResponse(Buffer.from(`md5${salted}\0`)); // Add md5 to the front and terminate with a null byte, et voila! A complete MD5-hashed password for the auth response.
+            } else { this.end(); throw(Error(`Authentication method ${authResult} not supported`)); } // Other auth mechanisms are not implemented.
             break;
-        case 78: // NoticeResponse
-            if (this.onNotice) this.onNotice(buf);
+        case 78: // NoticeResponse -- Received a notice from the server. Pass it onto an onNotice handler if there is one.
+            if (this.onNotice) this.onNotice(buf); // Call any possible onNotice handler with the message buffer.
             break;
-        case 65: // NotificationResponse
-            if (this.onNotification) this.onNotification(buf);
+        case 65: // NotificationResponse -- Received a notification message from the server. Pass it to a possible onNotification handler.
+            if (this.onNotification) this.onNotification(buf); // Call any possible onNotification handler with the message buffer.
             break;
-        case 75: // K -- BackendKeyData
-            this.backendKey = Buffer.from(buf.slice(off, off + length - 4));
+        case 75: // K -- BackendKeyData -- Server sent us a key to use for canceling requests on this connection.
+            this.backendKey = Buffer.from(buf.slice(off, off + length - 4)); // Store the key for later use in cancel().
             break;
-        case 118: // NegotiateProtocolVersion
-            this.end(); throw(Error('NegotiateProtocolVersion not implemented'));
-        case 86: // FunctionCallResponse -- Legacy, not supported.
-            this.end(); throw(Error('FunctionCallResponse not implemented'));
-        default:
-            console.error(cmd, String.fromCharCode(cmd), length, buf.toString('utf8', off, off + length - 4));
-            this.end(); throw(Error('Unknown message. Protocol state unknown, exiting.'));
+        case 118: // NegotiateProtocolVersion -- Server requests us to adapt to a different minor protocol version. We won't.
+            this.end(); throw(Error('NegotiateProtocolVersion not implemented')); // Terminate connection and bail out.
+        case 86: // FunctionCallResponse -- Legacy, not supported, use 'SELECT my_func()' instead. How did you manage to do a FunctionCall in the first place?
+            this.end(); throw(Error('FunctionCallResponse not implemented')); // Terminate connection and bail out.
+        default: // Unknown command. Our code is broken. Or the server is broken. Anyway, it's not ACID, eject eject eject.
+            console.error(cmd, String.fromCharCode(cmd), length, buf.toString('utf8', off, off + length - 4)); // Log the command to stderr.
+            this.end(); throw(Error('Unknown message. Protocol state unknown, exiting.')); // Terminate connection and bail out.
     } }
-    promise(stream, parsed) { return new Promise((resolve, reject) => this._outStreams.push({stream, parsed, resolve, reject})); }
+    promise(stream, parsed) { return new Promise((resolve, reject) => this._queryHandlers.push({stream, parsed, resolve, reject})); }
     parseAndDescribe(statementName, statement, types=[]) {
         const strBuf = Buffer.from(`${statementName}\0${statement}\0`);
         const len = 7 + strBuf.byteLength + types.length*4;
